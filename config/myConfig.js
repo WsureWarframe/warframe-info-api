dc3b06a6ee7dac243e7807f9e10e7590d9fee8f8
wsure <ws_1995@163.com>
2019-05-06 12:29:42 +0800

    加入https，为小程序提供后端

--- config/myConfig.js
+++ config/myConfig.js
@@ -1,7 +1,9 @@
 const MY_DEV = true;
 const LOCAL_LIB = true;
+const USE_HTTPS = true;
 myConfig = {
     localLib:LOCAL_LIB,
-    isDev:MY_DEV
+    isDev:MY_DEV,
+    https:USE_HTTPS
 };
 module.exports = myConfig;
