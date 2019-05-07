b08b9da390d6e7133d28991cd9d4136d1648c1f7
wsure <ws_1995@163.com>
2019-05-07 10:15:55 +0800

    优化https配置

--- config/myConfig.js
+++ config/myConfig.js
@@ -1,5 +1,8 @@
+//调试开关 ，目前貌似没用
 const MY_DEV = true;
+//中文词库配置 true为使用本地词库 false为启动时抓取wfa的词库
 const LOCAL_LIB = true;
+//https配置 如果使用Https，请手动修改./bin/www 内配置的证书，不用请
 const USE_HTTPS = true;
 myConfig = {
     localLib:LOCAL_LIB,
