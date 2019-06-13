54b50a0c11d0babaf80ae853caa5fb76ce090527
wsureDev <ws_1995@163.com>
2019-06-13 13:08:11 +0800

    update .gitignore

--- config/proxyConfig.js
+++
@@ -1,8 +0,0 @@
-const USE_PROXY = false;
-const PROXY_CONFIG = 'http://127.0.0.1:1081';
-proxyConfig = {
-    enabled:USE_PROXY,
-    proxy:PROXY_CONFIG,
-    config:USE_PROXY?PROXY_CONFIG:null
-};
-module.exports = proxyConfig;
