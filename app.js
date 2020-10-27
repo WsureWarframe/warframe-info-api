0cf9c1de5b27abff527b05307c33611be8455479
WangShuo <wangshuo@wayyue.com>
2020-10-27 12:44:10 +0800

    增加跨域

--- app.js
+++ app.js
@@ -37,6 +37,18 @@ init.onstart().then(() =>{
   app.use(function(req, res, next) {
     next(createError(404));
   });
+  app.all("*",function (req,res,next){
+    //设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Origin","*");
+    //允许的header类型
+    res.header("Access-Control-Allow-Headers","content-type");
+    //跨域允许的请求方式
+    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+    if (req.method.toLowerCase() === 'options')
+      res.send(200);  //让options尝试请求快速结束
+    else
+      next();
+  })
 
 // error handler
   app.use(function(err, req, res, next) {
